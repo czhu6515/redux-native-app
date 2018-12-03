386ff758e977c44e9d2af9c7464414de69ee1699
Chen <chenyu.zhu86@gmail.com>
2018-12-03 17:11:12 -0500

    added LibraryList component

---
+++ src/components/LibraryList.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import { Text } from 'react-native'
+
+
+class LibraryList extends Component {
+  render(){
+    return(
+      <Text>Awsome</Text>
+    )
+  }
+}
+
+export default LibraryList
