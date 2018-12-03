608405e426bac72f916266528c34409b956de331
Chen <chenyu.zhu86@gmail.com>
2018-12-03 16:05:12 -0500

    added reduc boilerplate

--- App.js
+++ App.js
@@ -1,21 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import reducers from './src/reducers/index'
 
-export default class App extends React.Component {
+class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
+      <Provider store={createStore(reducers)}>
         <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      </Provider>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
