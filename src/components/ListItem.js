4cfef5e59fafb1967fb98e5238760b1cab8fe6a5
Chen <chenyu.zhu86@gmail.com>
2018-12-04 20:04:25 -0500

    linked list item to selection reducer

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,18 +1,27 @@
 import React, {Component} from 'react'
-import { Text } from 'react-native'
+import { Text, TouchableWithoutFeedback, View } from 'react-native'
+import { connect } from 'react-redux'
 import { CardSection } from './common/index'
+import * as actions from '../actions/index'
 
 class ListItem extends Component {
   
   render(){
     const { titleStyle } = styles
+    const { id, title } = this.props.library
 
     return(
-      <CardSection>
-        <Text style={titleStyle}>
-          {this.props.library.title}
-        </Text>
-      </CardSection>
+      <TouchableWithoutFeedback 
+        onPress={() => this.props.selectLibrary(id)}
+      >
+        <View>
+          <CardSection>
+            <Text style={titleStyle}>
+              {title}
+            </Text>
+          </CardSection>
+        </View>
+      </TouchableWithoutFeedback>
     )
   }
 }
@@ -24,4 +33,4 @@ const styles = {
   }
 }
 
-export default ListItem
+export default connect(null, actions)(ListItem)
